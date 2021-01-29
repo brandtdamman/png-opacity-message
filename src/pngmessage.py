95d2c2d6243cc995c5d68ca2db6ea5f6bd8724e6
Brandt Damman <brandt.damman@spudwilson.us>
2021-01-29 11:03:00 -0600

    Remove testing code.

--- src/pngmessage.py
+++ src/pngmessage.py
@@ -11,7 +11,7 @@ _parser.add_argument('-t', '--input', type=str, help='Input text file to \
 _parser.add_argument('-o', '--output', type=str, help='Location and filename for \
     desired output file.')
 _parser.add_argument('-i', '--source', type=str, help='Location and filename for \
-    desired input PNG.  Please input a PNG only.')
+    desired input PNG.  Please input a PNG only.', required=True)
 
 def message_encoder(TextFile, AllowedCharacters):
     """Takes the source text file and converts to 8-bit binary.
@@ -60,10 +60,13 @@ def read_source(SourceFile):
         dataLength      --      allocated information limit
     """
     reader = png.Reader(filename=SourceFile)
-    pngData = reader.asRGBA()
-    print(pngData)
-    exit()
-    return (None, 0)
+    pngData = reader.asRGBA8()
+    if pngData[0] % 8 != 0:
+        # Seems arbitrary but necessary.
+        print("Image is not divisible by 8.")
+        exit()
+
+    return (pngData, pngData[0] / 8 * pngData[1])
 
 def write_target(TargetFile, BinaryData, PNGData):
     """Performs message encoding on PNG data whilst creating
@@ -76,9 +79,22 @@ def write_target(TargetFile, BinaryData, PNGData):
     Returns:
         encodedPNG      --      encoded PNG file to write
     """
+    # Data format: make list from index 2 generator, every index 3 is alpha
+    pngRGBA = list(PNGData[2])
+    charactersPerWidth = PNGData[0] / 8
+    counter = 0
+
+    for line in pngRGBA:
+        index = 3
+        while counter < charactersPerWidth:
+            # Alter alpha channel
+            # TODO: FIX IT.  FIX THE TEXT LIST.  NO STRINGS PLS
+            # line[index] =
+    
     return (None)
 
 if __name__ == "__main__":
+    print("Warning, your image's opacity values _will_ be altered.")
     args = _parser.parse_args()
     pngData, dataLength = read_source(args.source)
     binaryData = message_encoder(args.input, dataLength)
